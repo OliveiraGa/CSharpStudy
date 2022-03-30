3dc3847ee15c6d4de946b632a94393392c429955
Gabriel Oliveira <36790447+OliveiraGa@users.noreply.github.com>
2022-03-30 17:16:27 -0300

    C#

---
+++ CertificacaoCSharp/CertificacaoCsharp/CertificacaoCsharp/TiposValor/TipoDeValor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CertificacaoCsharp.TiposValor
+{
+    class TipoDeValor
+    {
+        public void Executar()
+        {
+            int idade = 30;
+            Console.WriteLine(idade);
+        }
+    }
+}

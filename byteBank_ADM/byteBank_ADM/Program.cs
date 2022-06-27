f442b0ee1e9d65ad3e2935b14bea48b5b6c3b977
Gabriel Oliveira <augusto.gabrielmorais@gmail.com>
2022-06-27 12:15:57 -0300

    Criação Projeto Byte Bank Adm

---
+++ byteBank_ADM/byteBank_ADM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace byteBank_ADM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
